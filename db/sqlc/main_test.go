ab1c3b35277b15351b872cb2aa5c18317dffd703
taufiqdp <topexdw@gmail.com>
2024-08-14 10:37:06 +0700

    Update main test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,15 +10,17 @@ import (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open("postgres", "postgresql://root:pwd@127.0.0.1:5432/simplebank?sslmode=disable")
+	var err error
+	testDB, err = sql.Open("postgres", "postgresql://root:pwd@127.0.0.1:5432/simplebank?sslmode=disable")
 
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
