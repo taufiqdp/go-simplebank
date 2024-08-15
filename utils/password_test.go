917994718aa67e36ef3226ba0cb80a9f9765750b
taufiqdp <topexdw@gmail.com>
2024-08-15 15:02:06 +0700

    update: add test hashed password should not equal

--- utils/password_test.go
+++ utils/password_test.go
@@ -21,4 +21,10 @@ func TestPassword(t *testing.T) {
 	err = CheckPassword(wrongPassword, hashedPassword)
 	require.Error(t, err)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword2)
+
+	require.NotEqual(t, hashedPassword, hashedPassword2)
 }
