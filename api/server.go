b8c0cbf4396aed4c963df0845db018e5f3bd9c17
taufiqdp <topexdw@gmail.com>
2024-08-16 11:42:52 +0700

    update: add paseto token maker

--- api/server.go
+++ api/server.go
@@ -1,21 +1,36 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/taufiqdp/go-simplebank/db/sqlc"
+	"github.com/taufiqdp/go-simplebank/token"
+	"github.com/taufiqdp/go-simplebank/utils"
 )
 
 type Server struct {
-	db     db.Store
-	router *gin.Engine
+	config     utils.Config
+	db         db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{db: store}
+func NewServer(config utils.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		db:         store,
+		tokenMaker: tokenMaker,
+	}
+
 	gin.SetMode(gin.DebugMode)
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -41,7 +56,7 @@ func NewServer(store db.Store) *Server {
 	engine.POST("/transfers", server.CreateTransfer)
 
 	server.router = engine
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(addr string) error {
